aee02f79e79fe98be8fe77a025f398eaaf82ce1f
Justas Grinaveckis <grinaveckisjustas@gmail.com>
2023-03-20 21:59:04 +0200

    first task

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,5 +14,33 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Hello World';
 });
+
+Route::get('/{name}/', function (string $name) {
+    return $name;
+});
+
+Route::get('/{type}/{first}/{second}', function (string $type,string $first, string $second) {
+    if ($type == "sum") {
+        $sum = intval($first) + intval($second);
+        return $sum;
+    }
+    elseif ($type == "subtract") {
+        $sum = intval($first) - intval($second);
+        return $sum;
+    }
+    elseif ($type == "multiply") {
+        $sum = intval($first) * intval($second);
+        return $sum;
+    }
+    elseif ($type == "divide") {
+        $sum = intval($first) / intval($second);
+        return $sum;
+    }
+    else {
+        return "Please provide argument";
+    }
+
+})->where(['type' => '[a-z]+', 'first' => '[0-9]+', 'second' => '[0-9]+']);
+
