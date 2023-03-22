8ea8cba2264aa00f4a1a5261f9f2f4c9bcec1bbf
Justas Grinaveckis <grinaveckisjustas@gmail.com>
2023-03-22 20:04:21 +0200

    some changes to infra

--- src/routes/web.php
+++ src/routes/web.php
@@ -25,5 +25,7 @@ Route::get('/{name}/', [GreetingController::class, 'greet']);
 Route::get('/{type}/{first}/{second}', [CalculatorController::class, 'calculate']
 )->where(['type' => '[a-z]+', 'first' => '[0-9]+', 'second' => '[0-9]+']);
 
-Route::resource('photos', 'PhotosController');
-
+Route::get('/tasks', [TaskController::class, 'list']);
+Route::post('/tasks', [TaskController::class, 'create']);
+Route::get('/tasks/{id}', [TaskController::class, 'find'])->whereNumber('id');
+Route::post('/tasks/{id}', [TaskController::class, 'update']);
